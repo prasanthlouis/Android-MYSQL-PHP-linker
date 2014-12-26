1126c0c745ef94ec9e6ab65ff9ae1fa7906d65f3
Prasanth Louis <prasanthlouis21@gmail.com>
2014-12-26 19:07:27 +0530

    Create database_configuration.php

---
+++ database_configuration.php
@@ -0,0 +1,7 @@
+<?php
+//basically just creating some constants to represent values
+define('DB_USER',"me");
+define('DB_PASSWORD',"notme");
+define('DB',"android_db");
+define('DB_SERVER',"localhost");
+?>
