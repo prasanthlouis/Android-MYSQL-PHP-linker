f14653dff6e136fc39b1d14950fe83e05cb99dd2
Prasanth Louis <prasanthlouis21@gmail.com>
2015-01-02 19:34:10 +0530

    Update product.php

--- product.php
+++ product.php
@@ -6,11 +6,13 @@ if((isset($_POST['name'])) && (isset($_POST['price'])) && (isset($_POST['des']))
 $name=$_POST['name'];
 $price=$_POST['price'];
 $des=$_POST['des']; 
-require_once __DIR__ . '/db_connect.php';
+require_once __DIR__ . '/connection.php';
 $db=new DB_CONNECT();
 $result=my_sqlquery("INSERT INTO products(name,price,description) VALUES ('$name','$price','$des')");
 if($result)
 {
+  //NOTE: DO NOT REMOVE THE STATUS VARIABLES. By using the JSONResponse, we're checking if a row has been entered or not in
+  //the android app.
 response["success"]=1;
 response["message"]="It has been entered";
 }
