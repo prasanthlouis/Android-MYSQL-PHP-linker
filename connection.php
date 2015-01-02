ec3f0fe3bd3c68961baa5352be61723725c27f69
Prasanth Louis <prasanthlouis21@gmail.com>
2015-01-02 19:32:07 +0530

    Update and rename database_connect.php to connection.php

--- connection.php
+++ connection.php
@@ -1,20 +1,26 @@
 <?php
 class DB_CONNECT {
-    function __construct() { //this is the constructor function
+
+    function __construct() {
         $this->connect();
     }
-    function __destruct() {  //this is the destructor function
+
+    function __destruct() {
         $this->close();
     }
- 
+
     function connect() {
-         require_once __DIR__ . '/database_configuration.php';  //its checking to include the db_config file, and if already included, not to include it again
+  
+      require_once __DIR__ . '/configuration.php';  //its checking to include the db_config file, and if already included, not to include it again
 		$con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error()); //parameters are server, username, password
 		$db = mysql_select_db(DB) or die(mysql_error()) or die(mysql_error());  //selection of the database
 		return $con; //this is a cursor returned once connected
+        return $con;
     }
     function close() {
         mysql_close();
     }
+ 
 }
+ 
 ?>
